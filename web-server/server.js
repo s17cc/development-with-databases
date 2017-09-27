423f05966aea238f7eee5def96827c8284f18b63
Konstantin Urysov <konstantin.urysov@akvelon.com>
2017-09-27 09:58:47 +0300

    lab preparation
    fixing the code

--- web-server/server.js
+++ web-server/server.js
@@ -1,12 +1,14 @@
-var config = require("./config");
-var db = require("knex")(config);
+// var config = require("./config");
+// var db = require("knex")(config);
 var express = require("express");
 
 express()
  .get("*", function(req, res) {
-    db("users").then(function(data) {
-      res.send(data);
-    });
+    res.send("ok");
+
+    // db("users").then(function(data) {
+    //   res.send(data);
+    // });
  })
  .listen(80, function(err) {
     if (err) {
